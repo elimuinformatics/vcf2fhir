c83854c32e9fffad2861bcce778a718e767e4f14
Shailesh Gothi <shaileshgothi@ymail.com>
2020-12-05 20:16:29 -0500

    Removed boolean return from convert method
    Updated version to 0.0.16

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vcf2fhir",
-    version="0.0.11",
+    version="0.0.16",
     author="",
     test_suite='vcf2fhir.test.test_vcf2fhir.suite',
     author_email="info@elimu.io",
