96833dea9ee3542fa140125126e2f7738ca77490
rohangupta <rohaninjmu@gmail.com>
2021-03-26 15:13:37 +0530

    Refactored python code so that it conforms to PEP8
    Used 'autopep8' to refactor the complete python code so that it conforms to PEP8.
    Fixes #35.
    
    Refactored code to remove E501(line too long) error and updated function, and variable names
    
    Resolved Git Conflicts

--- vcf2fhir/common.py
+++ vcf2fhir/common.py
@@ -13,20 +13,20 @@ class _Utilities(object):
 
     """
     /**
-     * generates current date into fhir format 
+     * generates current date into fhir format
      * @return date
      */
     """
-    def getFhirDate():
+    def get_fhir_date():
         z = datetime.datetime.now(pytz.timezone(
             'UTC')).strftime("%Y-%m-%dT%H:%M:%S%z")
-        return z[:-2]+':'+z[-2:]
+        return z[:-2] + ':' + z[-2:]
 
-    def getSequenceRelation(phasedRecMap):
-        RelationTable = pd.DataFrame(columns=['POS1', 'POS2', 'Relation'])
-        for key in phasedRecMap:
+    def get_sequence_relation(phased_rec_map):
+        Relation_table = pd.DataFrame(columns=['POS1', 'POS2', 'Relation'])
+        for key in phased_rec_map:
             prev_record = None
-            for record in phasedRecMap[key]:
+            for record in phased_rec_map[key]:
                 if prev_record is None:
                     prev_record = record
                     continue
@@ -34,45 +34,63 @@ class _Utilities(object):
                 record_data = record.samples[0].data
                 if(prev_data.PS == record_data.PS):
                     if prev_data.GT == record_data.GT:
-                        RelationTable = RelationTable.append(
-                            {'POS1': prev_record.POS, 'POS2': record.POS, 'Relation': 'Cis'}, ignore_index=True)
+                        Relation_table = Relation_table.append(
+                            {
+                                'POS1': prev_record.POS,
+                                'POS2': record.POS,
+                                'Relation': 'Cis'
+                            },
+                            ignore_index=True
+                        )
                     else:
-                        RelationTable = RelationTable.append(
-                            {'POS1': prev_record.POS, 'POS2': record.POS, 'Relation': 'Trans'}, ignore_index=True)
+                        Relation_table = Relation_table.append(
+                            {
+                                'POS1': prev_record.POS,
+                                'POS2': record.POS,
+                                'Relation': 'Trans'
+                            },
+                            ignore_index=True
+                        )
                 prev_record = record
-        return RelationTable
+        return Relation_table
 
-    def getAllelicState(record, ratio_ad_dp):
-        allelicState = ''
-        allelicCode = ''
+    def get_allelic_state(record, ratio_ad_dp):
+        allelic_state = ''
+        allelic_code = ''
         # Using  the first sample
         sample = record.samples[0]
         alleles = sample.gt_alleles
         if record.CHROM != 'M':
             if len(alleles) >= 2 and sample.gt_type == 1:
-                allelicState = 'heterozygous'
-                allelicCode = 'LA6706-1'
+                allelic_state = 'heterozygous'
+                allelic_code = 'LA6706-1'
             elif len(alleles) >= 2:
-                allelicState = 'homozygous'
-                allelicCode = 'LA6705-3'
-            elif sample.gt_type != None and len(alleles) == 1:
-                allelicState = 'hemizygous'
-                allelicCode = 'LA6707-9'
+                allelic_state = 'homozygous'
+                allelic_code = 'LA6705-3'
+            elif sample.gt_type is not None and len(alleles) == 1:
+                allelic_state = 'hemizygous'
+                allelic_code = 'LA6707-9'
             else:
                 _Utilities._error_log_allelicstate(record)
-        elif sample.gt_type != None and len(alleles) == 1 and alleles[0] == '1':
+        elif (
+                sample.gt_type is not None and
+                len(alleles) == 1 and
+                alleles[0] == '1'):
             if hasattr(sample.data, 'AD') and hasattr(sample.data, 'DP'):
                 try:
-                    if(isinstance(sample.data.AD, list) and len(sample.data.AD) > 0):
-                        ratio = float(sample.data.AD[0])/float(sample.data.DP)
+                    if(
+                            isinstance(sample.data.AD, list) and
+                            len(sample.data.AD) > 0):
+                        ratio = float(
+                            sample.data.AD[0]) / float(sample.data.DP)
                     else:
-                        ratio = float(sample.data.AD)/float(sample.data.DP)
+                        ratio = float(sample.data.AD) / float(sample.data.DP)
                     if ratio > ratio_ad_dp:
-                        allelicState = "homoplasmic"
-                        allelicCode = "LA6704-6"
+                        allelic_state = "homoplasmic"
+                        allelic_code = "LA6704-6"
                     else:
-                        allelicState = "heteroplasmic"
-                        allelicCode = "LA6703-8"
+                        allelic_state = "heteroplasmic"
+                        allelic_code = "LA6703-8"
                 except Exception as e:
                     general_logger.debug(e)
                     _Utilities._error_log_allelicstate(record)
@@ -81,7 +99,7 @@ class _Utilities(object):
                 _Utilities._error_log_allelicstate(record)
         else:
             _Utilities._error_log_allelicstate(record)
-        return {'ALLELE': allelicState, 'CODE': allelicCode}
+        return {'ALLELE': allelic_state, 'CODE': allelic_code}
 
     def extract_chrom_identifier(chrom):
         chrom = chrom.upper().replace("CHR", "")
@@ -111,4 +129,6 @@ class _Utilities(object):
 
     def _error_log_allelicstate(record):
         general_logger.error(
-            "Cannot Determine AllelicState for: %s , considered sample: %s", record, record.samples[0].data)
+            "Cannot Determine AllelicState for: %s , considered sample: %s",
+            record,
+            record.samples[0].data)
