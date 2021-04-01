4189dc823d9c71946f3945ca61f65147c2ac8f27
theanmolsharma <65589791+theanmolsharma@users.noreply.github.com>
2021-04-01 14:26:32 +0530

    Issue #36: Remove 'Human reference sequence assembly version' component from variant observation
    Removed the code that created the component LOINC 62374-4

--- vcf2fhir/fhir_helper.py
+++ vcf2fhir/fhir_helper.py
@@ -124,13 +124,6 @@ class _Fhir_Helper:
             {"coding": [{"system": "http://loinc.org", "code": "LA9633-4", "display": "present"}]})
         observation_dv.component = []
 
-        observation_dv_component1 = observation.ObservationComponent()
-        observation_dv_component1.code = concept.CodeableConcept(
-            {"coding": [{"system": "http://loinc.org", "code": "62374-4", "display": "Human reference sequence assembly version"}]})
-        observation_dv_component1.valueCodeableConcept = concept.CodeableConcept(
-            {"coding": [{"system": "http://loinc.org", "code": "LA14029-5", "display": "GRCh37"}]})
-        observation_dv.component.append(observation_dv_component1)
-
         observation_dv_component2 = observation.ObservationComponent()
         observation_dv_component2.code = concept.CodeableConcept(
             {"coding": [{"system": "http://loinc.org", "code": "48013-7", "display": "Genomic reference sequence ID"}]})
